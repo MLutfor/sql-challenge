ebf31788b1f6f686085dcfd2ab391e6d0142cee2
MLutfor <142198802+MLutfor@users.noreply.github.com>
2023-10-19 18:28:23 -0400

    g

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -1,44 +1,29 @@
 CREATE TABLE departments (
-    department_id VARCHAR(4) PRIMARY KEY,
-    department_name VARCHAR(40)
+    
 );
 
 CREATE TABLE dept_emp (
-    emp_no INT,
-    department_id VARCHAR(4),
-    from_date DATE,
-    to_date DATE
+    
 );
 
 CREATE TABLE dept_manager (
-    emp_no INT,
-    department_id VARCHAR(4),
-    from_date DATE,
-    to_date DATE
+    emp_no VARCHAR(20),
+    dept_no INT
 );
 
 CREATE TABLE employees (
-    emp_no INT PRIMARY KEY,
-    birth_date DATE,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    gender CHAR(1),
-    hire_date DATE
+    
 );
 
 CREATE TABLE salaries (
-    emp_no INT,
-    salary INT,
-    from_date DATE,
-    to_date DATE
+    
 );
 
 CREATE TABLE titles (
-    emp_no INT,
-    title VARCHAR(50),
-    from_date DATE,
-    to_date DATE
+
 );
 
--- Import the "departments" CSV
-COPY departments FROM 'csv_files/departments.csv' WITH CSV HEADER;
+
+SELECT * FROM titles
+LIMIT 5;
+

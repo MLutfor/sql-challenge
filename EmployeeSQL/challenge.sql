dd8eb126c14c3cb85adef8fa6c98cdc6ca3a7e45
MLutfor <142198802+MLutfor@users.noreply.github.com>
2023-10-21 20:30:01 -0400

    f

--- EmployeeSQL/challenge.sql
+++ EmployeeSQL/challenge.sql
@@ -1,31 +1,25 @@
 CREATE TABLE departments (
-    dept_no INT,
+    dept_no VARCHAR(20),
     dept_name VARCHAR(20)
 );
 
 CREATE TABLE dept_emp (
     emp_no VARCHAR(20),
-    dept_no INT
+    dept_no VARCHAR(20)
 );
 
 CREATE TABLE dept_manager (
     emp_no VARCHAR(20),
-    dept_no INT
+    dept_no VARCHAR(20)
 );
 
 CREATE TABLE employees (
-    
+	#convert date from string to date
 );
 
 CREATE TABLE salaries (
-    
+
 );
 
 CREATE TABLE titles (
-
 );
-
-
-SELECT * FROM titles
-LIMIT 5;
-
